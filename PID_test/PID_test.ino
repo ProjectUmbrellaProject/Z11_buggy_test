a5ddf22c1de3a85610a57cd94a51c44d3bcc9206
Cian Levy <cian.b.levy@gmail.com>
2018-03-24 11:48:49 +0000

    PID tuning updates
    Updated gain values from the lab
    Added (broken) obstacle detection

--- PID_test/PID_test.ino
+++ PID_test/PID_test.ino
@@ -6,13 +6,25 @@
 #define eyeOne 5 //Originally left override
 //#define eyeTwo to be determined
 //#define eyeThree to be determined
-#define eyeFour 6 //Originally right override
+#define eyeFour 0 //Originally right override
+#define echoPin 8
+#define trigPin 9
 
 String inputString = "";
 bool stringComplete, start;
+//d = 0.15, p = 0.04  or  K_d = 0.11, K_p = 0.055, 
+//most successsful value: Kd=0.11, kp = 0, basespeed = 240
 
-const float K_d = 100, K_p = 1, setPoint = 2500; //K_p < K_d the derivative term must be large to have a significant influence
-int maxMotorSpeed = 255, baseSpeed = 200;
+const float K_d = 0.15, K_p = 0.04, setPoint = 2500; //K_p < K_d the derivative term must be large to have a significant influence
+int maxMotorSpeed = 255, baseSpeed = 240;
+
+unsigned long previousPingTime;
+const short pingInterval = 400;
+const short minimumDistance = 15; //Determines how close an object must be to stop the buggy
+bool objectDetected;
+
+    int rightMotorSpeed;
+    int leftMotorSpeed;
 
 void setup() {
   pinMode(leftMotorDirection, OUTPUT);
@@ -20,6 +32,8 @@ void setup() {
   pinMode(rightMotorDirection, OUTPUT);
   pinMode(rightMotorSpeedPin, OUTPUT);
   pinMode(speedPin, OUTPUT);
+  pinMode(eyeOne, INPUT);
+  pinMode(eyeFour, INPUT);
   
   digitalWrite(speedPin, HIGH);
   analogWrite(rightMotorSpeedPin, 0);
@@ -42,12 +56,16 @@ void setup() {
 }
 
 void loop() {
+ 
+
   if (stringComplete){
-    
-    if (inputString == "/0")
-      start = false;
-    else if (inputString == "/1")
-      start = true;
+    int command = inputString.toInt();
+      if (command == 0)
+        start = false;
+      else if (command == 1)
+        start = true;
+
+    Serial.println("Starting");
 
     inputString = "";
     stringComplete = false;
@@ -61,8 +79,21 @@ void loop() {
     int motorSpeed = K_p * error + K_d * (error - previousError);
     previousError = error;
   
-    int rightMotorSpeed = baseSpeed + motorSpeed;
-    int leftMotorSpeed = baseSpeed - motorSpeed;
+    rightMotorSpeed = baseSpeed + motorSpeed;
+    leftMotorSpeed = baseSpeed - motorSpeed;
+
+      unsigned long currentTime = millis(); //Update the time variable with the current time
+  if (currentTime - previousPingTime >= pingInterval){
+    previousPingTime = currentTime; 
+    Serial.print("left: ");
+    Serial.print(leftMotorSpeed);
+    Serial.print(" right: ");
+    Serial.println(rightMotorSpeed);
+   
+        
+  //  handleObjectDetection();
+  }
+
   
     //Constrain motors to a range of output between 0 and maxMotorSpeed
     if (rightMotorSpeed > maxMotorSpeed)
@@ -75,21 +106,30 @@ void loop() {
     else if (leftMotorSpeed < 0)
       leftMotorSpeed = 0;
   
-      digitalWrite(speedPin, HIGH);
+      //digitalWrite(speedPin, HIGH);
+      analogWrite(speedPin, 255);
       digitalWrite(rightMotorDirection, LOW);
       analogWrite(rightMotorSpeedPin, rightMotorSpeed);
       digitalWrite(leftMotorDirection, LOW);
       analogWrite(leftMotorSpeedPin, leftMotorSpeed);
+  } else{
+
+  analogWrite(rightMotorSpeedPin, 0);
+  analogWrite(rightMotorDirection, 0);
+  analogWrite(leftMotorDirection, 0);
+  analogWrite(leftMotorSpeedPin, 0);
+
   }
   getSensorValue(); //The sensor value must be updated as frequently as possible for this to work
+
 }
 
 
 float getSensorValue(){
   //Do something to combine the values of all IR sensors into one number
   int eyeOutputs[4];
-  eyeOutputs[0] = analogRead(eyeOne);
-  eyeOutputs[3] = analogRead(eyeFour);
+  eyeOutputs[3] = analogRead(eyeOne);
+  eyeOutputs[0] = analogRead(eyeFour);
 
   float k[2] = {3.125, 1.5635}; //Constants to constrain output within range 0 to 2*setpoint
 
@@ -98,8 +138,8 @@ float getSensorValue(){
    *  50 < Output < 300 Black
    *  300 < Output < 900 White
    */
-  float output = -k[0] * eyeOutputs[0] - k[1] * eyeOutputs[1] + k[1] * eyeOutputs[2] + k[0] * eyeOutputs[3] + setPoint;
-  
+ // float output = -k[0] * eyeOutputs[0] - k[1] * eyeOutputs[1] + k[1] * eyeOutputs[2] + k[0] * eyeOutputs[3] + setPoint;
+  float output = -k[0] * eyeOutputs[0] + k[0] * eyeOutputs[3] + setPoint;
   return output;
  
 }
@@ -115,3 +155,60 @@ void serialEvent() {
     }
   }
 }
+void handleObjectDetection(){
+    long distance = obstacleDistance();
+
+    //The command to stop should only be called if the control program has told the buggy to move and an object hasnt already been detected
+    //i.e. the buggy isnt already stationary
+    if (!objectDetected && distance <= minimumDistance ){
+      
+      objectDetected = true; //The objectDetected boolean prevents the if statement from being repeatedly executed while the object is still present
+      moveCommand(0);
+     // forward = true; //Calling move command 0 also sets forward to false which is unintended in this case
+      Serial.print("~6 ");
+      Serial.println(distance);
+
+    }
+    else if (objectDetected && distance > minimumDistance){
+        objectDetected = false;
+        moveCommand(1);
+    }
+}
+
+//This function returns the distance to the nearest object as measured by the ultrasonic sensor
+long obstacleDistance(){
+  long distance, duration;
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance = round(duration*0.034/2);
+
+  return distance;
+
+  
+}
+
+void moveCommand(int command){
+
+  if (command == 1){
+    start = true;
+
+  }
+  else if (command == 0)
+    start = false;
+
+
+
+}
+
